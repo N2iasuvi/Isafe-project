721e8f4a91bb6bd2ee8a0d2c14e44a6734d9c1a4
N2iasuvi <119475306+N2iasuvi@users.noreply.github.com>
2022-12-04 22:58:02 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <title>Document</title> -->
+</head>
+<body>
+
+</body>
+<style>
+    body{
+      margin:0;
+      padding:0;
+      font-family: 'Times New Roman', Times, serif;
+      background-color:rgb(40,42,53);
+       height:100vh;
+       overflow: hidden;
+    }
+  
+     h1{
+        color:orange;
+        text-align:center;
+        border:10px solid tomato;
+        border-radius:60px;
+        margin-top :20px;
+        margin-left:400px;
+        margin-right:400px;
+        margin-bottom:10px;
+        background:black;
+    
+
+    }
+    h2{
+        text-align:center;
+    }
+</style>
+</html>
